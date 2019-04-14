ef13fb873e6ce2fc7736e63bdf4c2fdb6ddef261
Fady Shenoda <fady@Fadys-MacBook-Pro.local>
2019-04-14 12:57:41 -0400

    created game logic with comparing cards part

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,24 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// make an array of cards and an empty array
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
+// choose two random cards
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+// add the cards to the play array
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+// check your work
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
 
+//checking if two cards were chosen, then checking if they match
+if(cardsInPlay.length === 2 ){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}else{
+		alert("Sorry, try again");
+	}
+}
